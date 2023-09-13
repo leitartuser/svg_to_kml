b408b9385eadf8ca42b5be8eb99ae2567dcac357
ol'tobi bastard <106253616+leitartuser@users.noreply.github.com>
2023-09-13 13:30:46 +0200

    code cleaned

--- kmlfaster.py
+++ kmlfaster.py
@@ -1,6 +1,5 @@
 # Import the library
 from fastkml import kml
-from pygeoif.geometry import Polygon
 
 
 def create_kml(list_of_polygons):
@@ -24,44 +23,10 @@ def create_kml(list_of_polygons):
     f2 = kml.Folder(ns, 'id2', 'name2', 'description2')
     d.append(f2)
     for i, each in enumerate(list_of_polygons):
-        # Create a Placemark with a simple polygon geometry and add it to the
-        # second folder of the Document
-        p = kml.Placemark(ns, str(i), str(i), 'description')
-
-        print(each)
-        p.geometry = each
-        f2.append(p)
-
-    new_k = (k.to_string(prettyprint=True))
-    return k, new_k
-
-
-def new_create_kml(list_of_polygons):
-    # Create the root KML object
-    k = kml.KML()
-    ns = '{http://www.opengis.net/kml/2.2}'
-
-    # Create a KML Document and add it to the KML root object
-    d = kml.Document(ns, 'docid', 'doc name', 'doc description')
-    k.append(d)
-
-    # Create a KML Folder and add it to the Document
-    f = kml.Folder(ns, 'fid', 'f name', 'f description')
-    d.append(f)
-
-    # Create a KML Folder and nest it in the first Folder
-    nf = kml.Folder(ns, 'nested-fid', 'nested f name', 'nested f description')
-    f.append(nf)
-
-    # Create a second KML Folder within the Document
-    f2 = kml.Folder(ns, 'id2', 'name2', 'description2')
-    d.append(f2)
-    for i, each in enumerate(list_of_polygons):
-        print(each)
-        # print(type())
-        # Create a Placemark with a simple polygon geometry and add it to the
-        # second folder of the Document
+        # Create a Placemark with a simple polygon geometry and add it to the second folder of the Document
         id = each['id']
+        if len(id) == 0:
+            id = str(i)
 
         p = kml.Placemark(ns, id, id, 'description')
 
@@ -71,4 +36,3 @@ def new_create_kml(list_of_polygons):
     new_k = (k.to_string(prettyprint=True))
     return k, new_k
 
-# k = create_kml([Polygon([(0, 0, 0), (1, 1, 0), (1, 0, 1)])])
--- svg_to_kml.py
+++ svg_to_kml.py
@@ -7,7 +7,6 @@ from svgpathtools import Path
 import geopandas as gpd
 from operator import itemgetter
 from kmlfaster import create_kml
-from kmlfaster import new_create_kml
 
 
 def find_svg():
@@ -23,9 +22,6 @@ def get_rectangles(svg_doc):
     polygon_with_id = []
     polygons = []
     for irect, rect in enumerate(svg_doc.getElementsByTagName('rect')):
-        # if irect > 0:
-        # x0 = int(rect.getAttribute('x'))
-        # y0 = int(rect.getAttribute('y'))
         x0 = float(rect.getAttribute('x'))
         y0 = float(rect.getAttribute('y'))
         id = rect.getAttribute('id')
@@ -38,25 +34,11 @@ def get_rectangles(svg_doc):
                            ])
         polygons.append(polygon)
         polygon_with_id.append({"id": id, "geometries": polygon})
-    # for isvg, svg in enumerate(svg_doc.getElementsByTagName('svg')):
-    #     if isvg == 0:
-    #         width = float(svg.getAttribute('width'))
-    #         height = float(svg.getAttribute('height'))
-    #         polygon = Polygon([(x0, y0),
-    #                            (x0 + width, y0),
-    #                            (x0 + width, y0 + height),
-    #                            (x0, y0 + height)
-    #                            ])
-    #         print("this is the biggest polygon")
-    #         print(polygon)
-    #         polygons.append(polygon)
-
     return polygons, polygon_with_id
 
 
 def get_paths(svg_doc):
-    new_whole_path = []
-    whole_path = []
+    all_paths = []
     for ipath, path in enumerate(svg_doc.getElementsByTagName('path')):
         print('Path', ipath)
         d = path.getAttribute('d')
@@ -73,28 +55,11 @@ def get_paths(svg_doc):
                     bezier = (bezier_path.point(i / (num_samples - 1)))
                     if bezier is not None:
                         circle_path.append(bezier)
-        whole_path.append(circle_path)
-        new_whole_path.append({"id": id, "geometries": circle_path})
-    return whole_path, new_whole_path
+        all_paths.append({"id": id, "geometries": circle_path})
+    return all_paths
 
 
 def attach_paths_to_polygon(whole_path, polygons):
-    for each_path in whole_path:
-        circle = []
-        for complex_num in each_path:
-            imaginary = complex_num.imag
-            real = complex_num.real
-            point = [float(real), float(imaginary)]
-            circle.append(point)
-        try:
-            polygono = Polygon(circle)
-            polygons.append(polygono)
-        except:
-            print("An exception occurred")
-    return polygons
-
-
-def new_attach_paths_to_polygon(whole_path, polygons):
     all_geometries = []
     for each_path in whole_path:
         circle = []
@@ -114,42 +79,16 @@ def new_attach_paths_to_polygon(whole_path, polygons):
 
 def show_coords(polygons, factor):
     for polygon in polygons:
-        # print(polygon)
-        x, y = polygon.exterior.xy
-        x_float = [float(line) for line in x]
-        y_float = [float(line) for line in y]
-        plt.plot(x_float, y_float, c="black", linewidth=0.5)
-    plt.xlim([0, 4 * 1000 / factor])
-    plt.ylim([-4 * 1000 / factor, 0])
-
-    return plt.show()
-
-
-def new_show_coords(polygons, factor):
-    for polygon in polygons:
-        # print(polygon)
         x, y = polygon['geometries'].exterior.xy
         x_float = [float(line) for line in x]
         y_float = [float(line) for line in y]
         plt.plot(x_float, y_float, c="black", linewidth=0.5)
     plt.xlim([0, 4 * 1000 / factor])
     plt.ylim([-4 * 1000 / factor, 0])
-
     return plt.show()
 
 
 def determine_min(shape):
-    gpdf = gpd.GeoDataFrame(columns=['id', 'distance', 'feature'], geometry=[*shape])
-    bounds = gpdf.geometry.apply(lambda x: x.bounds).tolist()
-    min_x, min_y, max_x, max_y = min(bounds, key=itemgetter(0))[0], min(bounds, key=itemgetter(1))[1], \
-                                 max(bounds, key=itemgetter(2))[2], max(bounds, key=itemgetter(3))[
-                                 3]
-    print('This is the min tupel')
-    print(min_x, min_y, max_x, max_y)
-    return min_x, min_y, max_x, max_y
-
-
-def new_determine_min(shape):
     test_list = []
     for each in shape:
         print(each['geometries'])
@@ -174,11 +113,10 @@ def shift_to_root(shape, diff_x, diff_y):
         new_y = [each_y - diff_y for each_y in y]
         poly = Polygon(zip(new_x, new_y))
         all_geometries.append({"id": polygons['id'], "geometries": poly})
-    print(all_geometries)
     return all_geometries
 
 
-def new_resize_polygons(list_of_poly, factor):
+def resize_polygons(list_of_poly, factor):
     all_geometries = []
     for i, polygons in enumerate(list_of_poly):
         xx, yy = polygons['geometries'].exterior.coords.xy
@@ -188,7 +126,6 @@ def new_resize_polygons(list_of_poly, factor):
         new_y = [each_y / - factor for each_y in y]
         poly = Polygon(zip(new_x, new_y))
         all_geometries.append({"id": polygons['id'], "geometries": poly})
-    print(all_geometries)
     return all_geometries
 
 
@@ -226,8 +163,6 @@ Let Höhe;
 
 def make_new_qlik_script():
     script_string = f""""
-
-
 modified_1:
 Load
 	Name,
@@ -259,7 +194,6 @@ Let Oben_Breite = Peek('max_y_Wert', 0, 'max_min_table');
 Let Links_Laenge = Peek('min_x_Wert', 0, 'max_min_table');
 Let Unten_Breite = Peek('min_y_Wert', 0, 'max_min_table');
 Let Rechts_Laenge = Peek('max_x_Wert', 0, 'max_min_table');
-
 """
     return script_string
 
@@ -279,29 +213,22 @@ for each in all_svgs:
     output_file_name = each[6:len(each)-4]
     doc = minidom.parse(f'.{each}')
     poly, polygon_with_id = get_rectangles(doc)
-    added_path, new_added_path = get_paths(doc)
+    new_added_path = get_paths(doc)
     pixels_width, pixels_height = get_pixels(doc)
     doc.unlink()
-    poly = attach_paths_to_polygon(added_path, poly)
-    new_poly = new_attach_paths_to_polygon(new_added_path, polygon_with_id)
-    # resized_poly = resize_polygons(poly, fac)
-    resized_poly = new_resize_polygons(new_poly, fac)
-    # minx, miny, maxx, maxy = determine_min(resized_poly)
-    minx, miny, maxx, maxy = new_determine_min(resized_poly)
-    new_polygons = shift_to_root(resized_poly, minx, maxy)
-    # new_polygons = resized_poly
+    new_poly = attach_paths_to_polygon(new_added_path, polygon_with_id)
+    resized_poly = resize_polygons(new_poly, fac)
+    minx, miny, maxx, maxy = determine_min(resized_poly)
+    # new_polygons = shift_to_root(resized_poly, minx, maxy)
+    new_polygons = resized_poly
     # insert_qlik_string = make_qlik_script(pixels_width, pixels_height, fac)
     insert_qlik_string = make_new_qlik_script()
     print(insert_qlik_string)
     # panda = gpd.GeoDataFrame(columns=['id', 'meta_x', 'meta_y'], geometry=[*new_polygons])
     # panda.to_file(f'.\\output{output_file_name}.kml', driver='LIBKML')
     # panda.to_file(f'.\\output{output_file_name}.geojson', driver="GeoJSON")
-    # kml, kml_string = create_kml(new_polygons)
-    print(new_polygons)
-    kml, kml_string = new_create_kml(new_polygons)
-    # print(kml_string)
-
+    kml, kml_string = create_kml(new_polygons)
     with open(f'./output/{output_file_name}.kml', 'w') as f:
         f.write(kml_string)
 
-new_show_coords(new_polygons, fac)
+show_coords(new_polygons, fac)
